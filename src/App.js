77b9d56ac3d673735db455a8b9b386ff00f93569
Manmeet Singh <manmeet1987@gmail.com>
2020-11-28 14:50:49 +0000

    Changes made to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Manmeet Singh.
+          Edit <code>src/App.js</code> and save to reload by Manmeet Singh Jenkins.
         </p>
         <p>
             Added one more line.
